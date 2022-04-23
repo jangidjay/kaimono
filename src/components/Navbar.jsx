24aaf9f53478f932e823a7b4ec69e1b67d7fa5ce
jangidjay <jangid321jay@gmail.com>
2022-04-23 12:58:29 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,102 @@
-import React from 'react'
-import styled from 'styled-components'
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    height: 60px;
-    background-color: black;
+  height: 90px;
+`;
 
-`
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+const MenuItem = styled.div`
+  font-size: 16px;
+  cursor: pointer;
+  margin: 5px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  border: none;
+  outline: none;
+  padding: 5px;
+  margin: 0px 10px 0 10px;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+  padding: 5px;
+  border-radius: 25px;
+`;
+
+const SignIn = styled.div`
+  font-size: 16px;
+  cursor: pointer;
+  margin-right: 20px;
+`;
+
+const Language = styled.span`
+  font-size: 14;
+  cursor: pointer;
+  margin-left: 20px;
+`;
 
 const Navbar = () => {
   return (
     <Container>
-        Navbar
+      <Wrapper>
+        <Left>
+          <Logo>KAIMONO.</Logo>
+        </Left>
+        <Center>
+          <MenuItem>HOME</MenuItem>
+          <MenuItem>COLLECTIONS</MenuItem>
+          <MenuItem>ABOUT</MenuItem>
+        </Center>
+        <Right>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray" }} />
+          </SearchContainer>
+          <SignIn>SIGN IN</SignIn>
+          <Badge badgeContent={4} color="primary">
+            <ShoppingCartOutlined />
+          </Badge>
+          <Language>EN</Language>
+        </Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: require("./images/cloath_01.jpeg"),
+    title: "BEST JAPANESE CLOTHES",
+    desc: "Find Best Clothes of Best Japanese Brands at One Place",
+    bg: "f5fafd",
+  },
+  {
+    id: 1,
+    img: require("./images/cloath_02.jpg"),
+    title: "POPULAR JAPANESE BRANDS",
+    desc: "Choose From More Than 100 Most Popular Brands",
+    bg: "fcf1ed",
+  },
+  {
+    id: 1,
+    img: require("./images/cloath_03.jpg"),
+    title: "POPULAR SALE!!!",
+    desc: "Now get 40% discount on your very first Purchase",
+    bg: "fbf0f4",
+  },
+];
